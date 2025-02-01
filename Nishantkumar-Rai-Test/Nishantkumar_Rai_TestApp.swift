033c4a502ea7a70459460919341183a04de9e4aa
Nishantkummar Rai <nishantrai.nishant@gmail.com>
2025-02-01 17:32:44 +0530

    Initial Commit

---
+++ Nishantkumar-Rai-Test/Nishantkumar_Rai_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nishantkumar_Rai_TestApp.swift
+//  Nishantkumar-Rai-Test
+//
+//  Created by Nishant Rai on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Nishantkumar_Rai_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
