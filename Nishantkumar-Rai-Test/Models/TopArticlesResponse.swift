706b60317a5c2e61bb2f616364de8eddcd594d91
Nishantkummar Rai <nishantrai.nishant@gmail.com>
2025-02-02 18:13:42 +0530

    Resolved warning

--- Nishantkumar-Rai-Test/Models/TopArticlesResponse.swift
+++ Nishantkumar-Rai-Test/Models/TopArticlesResponse.swift
@@ -12,8 +12,27 @@ struct TopArticlesResponse: Decodable {
 }
 
 struct Article: Identifiable, Decodable {
-    let id = UUID()
+    let id: UUID
     let title: String
     let description: String?
     let urlToImage: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, description, urlToImage
+    }
+    
+    init(title: String, description: String?, urlToImage: String?) {
+        self.id = UUID()
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = UUID()
+        self.title = try container.decode(String.self, forKey: .title)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.urlToImage = try container.decodeIfPresent(String.self, forKey: .urlToImage)
+    }
 }
